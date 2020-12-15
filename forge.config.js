e27d74c492d8f9ebd4b4896a56d1a1a54ca1f762
mellobacon <42365887+mellobacon@users.noreply.github.com>
2020-12-14 21:50:37 -0600

    fixed small error

--- forge.config.js
+++ forge.config.js
@@ -3,7 +3,6 @@ const path = require('path')
 module.exports = {
   "packagerConfig": {
     "icon": path.join(__dirname, "Icons", "Terminal.ico"),
-    "iconURL": ""
     //"platform": "linux" // Choices are 'darwin', 'mas', 'win32' or 'linux'
     //"arch": "arm64" // Choices are ia32, x64, armv7l, arm64
   },
@@ -12,6 +11,8 @@ module.exports = {
       "name": "@electron-forge/maker-squirrel",
       "config": {
         "setupIcon": path.join(__dirname, "Icons", "Terminal.ico"),
+        "iconURL": "https://raw.githubusercontent.com/mellobacon/Termello/master/Icons/Terminal.ico",
+        "name": "Termello",
         "authors": "Mellobacon",
         "description": "A terminal emulator made with Electron"
       }
