2e5da1f9d2fdc685f13e5ba3d2c1622d7da9505e
mellobacon <42365887+mellobacon@users.noreply.github.com>
2020-12-19 00:43:46 -0600

    Added menubar

--- main.js
+++ main.js
@@ -1,19 +1,39 @@
-const {app, BrowserWindow} = require("electron");
+const {app, BrowserWindow, Menu, MenuItem} = require("electron");
 if (require('electron-squirrel-startup')) return app.quit();
 
 let mainWindow;
-
+const menu = new Menu();
 
 function createWindow() {
     mainWindow = new BrowserWindow({
         autoHideMenuBar: true,
         height: 450,
         width: 800,
+        frame: false,
+        transparent: true,
         icon: __dirname + '/Icons/Terminal.ico',
         webPreferences: {
             nodeIntegration: true
         }
     });
+
+    const menu = Menu.buildFromTemplate([
+        {
+            label: 'File',
+            submenu: [
+                {label:'About'},
+                {label:'Exit'}
+            ]
+        },
+        {
+            label: "Edit",
+            submenu: [
+                {label: "Preferences"}
+            ]
+        }
+    ])
+    Menu.setApplicationMenu(menu); 
+
     mainWindow.loadURL(`file://${__dirname}/index.html`);
     mainWindow.on("closed", function() {
         mainWindow = null;
