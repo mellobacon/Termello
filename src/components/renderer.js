aad65417724b0aabc9b1831dc505de12d28f6704
mellobacon <liz6757@outlook.com>
2020-12-24 23:09:31 -0600

    Fixed menu bar not showing in build

--- src/components/renderer.js
+++ src/components/renderer.js
@@ -1,7 +1,60 @@
 const customTitlebar = require('custom-electron-titlebar');
+const { ipcRenderer, remote} = require('electron');
 
-new customTitlebar.Titlebar({
-    backgroundColor: customTitlebar.Color.fromHex('#444'),
-    icon: "../Icons/Terminal.ico",
-});
 
+function createSettingsWindow(){
+    let w = new remote.BrowserWindow({
+        autoHideMenuBar: true,
+        height: 400,
+        width: 500,
+        webPreferences: {
+            nodeIntegration: true,
+            enableRemoteModule: true
+        }
+    });
+    w.webContents.openDevTools();
+
+    w.loadFile("src/pages/preferences.html");
+    
+    w.on("closed", function(){
+        w = null;
+    });
+}
+
+
+ipcRenderer.once("create-titlebar", function(){
+    const menu = new remote.Menu();
+    menu.append(new remote.MenuItem({
+        label: "File",
+            submenu: [
+                {
+                    label: "About",
+                    click(){
+                        remote.shell.openExternal("https://github.com/mellobacon/Termello");
+                    }
+                },
+                {
+                    label: "Exit",
+                    click(){
+                        remote.app.quit();
+                    },
+                }
+            ]
+    }));
+    menu.append(new remote.MenuItem({
+        label: "Edit",
+            submenu: [
+                {
+                    label: "Preferences",
+                    click(){
+                        createSettingsWindow();
+                    },
+                }
+            ]
+    }));
+    new customTitlebar.Titlebar({
+        backgroundColor: customTitlebar.Color.fromHex('#444'),
+        icon: "../Icons/Terminal.ico",
+        menu: menu,
+    });
+})
