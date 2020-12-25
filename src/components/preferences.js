08522a90a05627274ea3411a68af9029f68935cf
mellobacon <liz6757@outlook.com>
2020-12-24 23:10:51 -0600

    Added code for font-size customization

--- src/components/preferences.js
+++ src/components/preferences.js
@@ -3,9 +3,18 @@ const electron = require("electron");
 const ipc = electron.ipcRenderer;
 
 const themes = document.querySelector("#themes");
+const fontsize = document.querySelector(".counter-num");
+const increment = document.querySelector("#ctrl-plus");
+const decrement = document.querySelector("#ctrl-minus");
 
 themes.addEventListener("change", function(){
     const theme = themes.options;
     const option = themes.selectedIndex;
     ipc.send("theme", theme[option].text);
+})
+increment.addEventListener("click", function(){
+    console.log("up");
+})
+decrement.addEventListener("click", function(){
+    console.log("down");
 })
